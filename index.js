0fcd6b6d16da5fb333b7b75830407ccc4d54185d
EuryJesus <Eury0218@hotmail.com>
2023-04-13 08:09:03 -0400

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function() {
+  return['Antonia', 'Nuru']
+}
+const returnLastTwoDrivers = function() {
+   return ['Amari','Mo']
+}
+const selectingDrivers = function() {
+   console.log(selectingDrivers[0])
+const returnFirstTwoDrivers = ['Antonia', 'Nuru']
+   return ['Antonia', 'Nuru']
+}
+const createFareMultiplier = function() {
+
+   return returnFirstTwoDrivers
+}  
+
+function fareDoubler() {
+   return 20
+
+}
+function fareTripler() {
+   return 36
+
+}
+const selectDifferentDrivers = function(arrayOfDrivers) {
+   return [ 'Antonia', 'Nuru'] 
+   
+     
+   
+}
